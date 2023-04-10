bd602073491d6e5d370eede4db5de8d1d79a39eb
Afifrifaie <afifrifaie@gmail.com>
2023-04-11 07:13:25 +0800

    about page content added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,41 @@
+import React from "react";
+import "bulma/css/bulma.min.css";
+
 export const About = () => {
-  return <div className="aboutContainer">About</div>;
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">About Us</h1>
+        <div className="content">
+          <div>
+            <p>
+              Welcome to our movie database! Our website is dedicated to providing movie lovers with an easy-to-use platform where they can explore a wide variety of movies and TV shows. Our database is constantly updated with the latest releases, as well as classic titles from the past.
+            </p>
+          </div>
+          <div>
+            <p>
+              Our goal is to create a one-stop-shop for movie fans, where they can find all the information they need about their favorite titles. From synopses to reviews and ratings, our movie database is designed to provide a comprehensive overview of every movies and TV show we feature.
+            </p>
+          </div>
+          <div>
+            <p>
+              Our team is comprised of passionate movie enthusiasts with years of experience in the industry. We understand what makes a great movie and are dedicated to sharing our knowledge and love of film with our users. Our goal is to make the movie-watching experience as enjoyable and seamless as possible.
+            </p>
+          </div>
+          <div>
+          </div>
+          <div>
+            <p>
+              Moving forward, we are committed to constantly updating and improving our movie database. We have big plans for the future, including adding new features like personalized recommendations, user-generated reviews, and more. We are excited to continue providing movie fans with the ultimate online movie-watching experience.
+            </p>
+          </div>
+          <div>
+            <p>
+              Finally, we would like to extend a heartfelt thank you to everyone who has helped make this project a reality, including our dedicated team and our loyal users. We couldn't have done it without you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
