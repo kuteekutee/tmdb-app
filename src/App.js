17a7215c04a1fed75b8acb4098dda4e449e50aab
kuteekutee <bernardsng@gmail.com>
2023-04-10 21:49:17 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { MovieRoutes } from "./routes/movieRoutes";
+import Layout from "./layouts/Layout";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <MovieRoutes />
+      </Layout>
+    </>
   );
 }
 
