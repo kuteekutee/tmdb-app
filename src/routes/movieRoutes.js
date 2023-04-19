812fca0d464eb34aea238d32c130090ce66838f2
kuteekutee <bernardsng@gmail.com>
2023-04-19 16:48:27 +0800

    movieRoute not found in deployment

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -47,19 +47,6 @@ export const MovieRoutes = () => {
           <Route path="sign-in" element={<SignIn />} />
           <Route path="about" element={<About />} />
         </Route>
-
-        {/* <Route path="/" element={<Dashboard />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/trending" element={<Trending />} />
-          <Route path="/top-rated-movies" element={<TopRatedMovies />} />
-          <Route path="/top-rated-tv" element={<TopRatedTV />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="/tv/:id" element={<TvDetails />} />
-          <Route path="/favourites" element={<Favourites />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/about" element={<About />} />
-        </Route> */}
       </Routes>
     </>
   );
